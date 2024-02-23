bdc93f21c78a13983f014add30d729258ee2abbe
santigt11 <santiagoguachizaca11@gmail.com>
2024-02-23 09:10:53 -0500

    cambio frmActividad

--- src/vista/FrmActividad.java
+++ src/vista/FrmActividad.java
@@ -4,17 +4,67 @@
  */
 package vista;
 
+import controlador.DAO.grafosEjemplo.ActividadDao;
+import controlador.TDA.listas.DynamicList;
+import java.util.Date;
+import java.util.Random;
+import javax.swing.JOptionPane;
+import modelo.Tarea;
+
 /**
  *
  * @author Santiago
  */
 public class FrmActividad extends javax.swing.JFrame {
 
+    private ActividadDao actividadControl = new ActividadDao();
+    private DynamicList<Tarea> tareas = new DynamicList<>();
+
     /**
      * Creates new form FrmActividad
      */
     public FrmActividad() {
         initComponents();
+    }
+
+    public Boolean verificar() {
+        return (!txtDescripcion.getText().trim().isEmpty()
+                && !txtNombre.getText().trim().isEmpty()
+                && !txtNroTareas.getText().trim().isEmpty());
+    }
+
+    private void limpiar() {
+        tbActividades.clearSelection();
+        txtDescripcion.setText("");
+        txtNombre.setText("");
+        txtNroTareas.setText("");
+        actividadControl.setActividad(null);
+    }
+
+    private void cargarDatosActividad() {
+        if (verificar()) {
+            actividadControl.getActividad().setNombre(txtNombre.getText());
+            actividadControl.getActividad().setNumTareas(Integer.parseInt(txtNroTareas.getText()));
+            actividadControl.getActividad().setDescripcion(txtDescripcion.getText());
+        }else{
+            JOptionPane.showMessageDialog(null, "Complete los campos necesarios");
+        }
+    }
+
+    private void generarTareas() {
+        Tarea tarea;
+        for (int i = 1; i <= Integer.parseInt(txtNroTareas.getText()); i++) {
+            tarea = new Tarea(i, "Descripcion " + i, generarFechaAleatoria(), generarFechaAleatoria(), "Titulo " + i);
+            tareas.add(tarea);
+        }
+    }
+
+    private static Date generarFechaAleatoria() {
+        Random random = new Random();
+        long inicio = System.currentTimeMillis() - 1000L * 60 * 60 * 24 * 365; // Hace un año
+        long fin = System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 365; // Dentro de un año
+        long fechaAleatoria = inicio + (long) (random.nextDouble() * (fin - inicio));
+        return new Date(fechaAleatoria);
     }
 
     /**
@@ -37,7 +87,7 @@ public class FrmActividad extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tbActividades = new javax.swing.JTable();
         btCancelar = new org.edisoncor.gui.button.ButtonAero();
-        btGuardar = new org.edisoncor.gui.button.ButtonAero();
+        btAdyacencias = new org.edisoncor.gui.button.ButtonAero();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -66,6 +116,11 @@ public class FrmActividad extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        tbActividades.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tbActividadesMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tbActividades);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -116,9 +171,19 @@ public class FrmActividad extends javax.swing.JFrame {
 
         btCancelar.setBackground(new java.awt.Color(255, 0, 0));
         btCancelar.setText("CANCELAR");
+        btCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btCancelarActionPerformed(evt);
+            }
+        });
 
-        btGuardar.setBackground(new java.awt.Color(51, 255, 0));
-        btGuardar.setText("GUARDAR");
+        btAdyacencias.setBackground(new java.awt.Color(0, 0, 255));
+        btAdyacencias.setText("AGREGAR ADYACENCIAS");
+        btAdyacencias.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btAdyacenciasActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -129,7 +194,7 @@ public class FrmActividad extends javax.swing.JFrame {
                 .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(btGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btAdyacencias, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -141,12 +206,25 @@ public class FrmActividad extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btGuardar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btAdyacencias, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(0, 14, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btAdyacenciasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btAdyacenciasActionPerformed
+        generarTareas();
+        new FrmGrafoTarea(actividadControl.getActividad(), tareas).setVisible(true);
+    }//GEN-LAST:event_btAdyacenciasActionPerformed
+
+    private void tbActividadesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbActividadesMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tbActividadesMouseClicked
+
+    private void btCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btCancelarActionPerformed
+        limpiar();
+    }//GEN-LAST:event_btCancelarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -184,8 +262,8 @@ public class FrmActividad extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private org.edisoncor.gui.button.ButtonAero btAdyacencias;
     private org.edisoncor.gui.button.ButtonAero btCancelar;
-    private org.edisoncor.gui.button.ButtonAero btGuardar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private org.edisoncor.gui.label.LabelHeader labelHeader1;
