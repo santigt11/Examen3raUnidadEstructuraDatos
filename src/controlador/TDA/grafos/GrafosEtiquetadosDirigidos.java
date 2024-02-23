dc21b708e375d1078b0c74525a7962900f4dd8d2
santigt11 <santiagoguachizaca11@gmail.com>
2024-02-23 08:04:05 -0500

    subida de grafos

--- src/controlador/TDA/grafos/GrafosEtiquetadosDirigidos.java
+++ src/controlador/TDA/grafos/GrafosEtiquetadosDirigidos.java
@@ -4,22 +4,27 @@
  */
 package controlador.TDA.grafos;
 
+import Controlador.TDA.Listas.ListaEnlazada;
 import controlador.TDA.grafos.exception.LabelEdgeException;
 import controlador.TDA.grafos.exception.VerticeException;
-import controlador.TDA.listas.DynamicList;
-import controlador.Utiles.Utiles;
+import controlador.utiles.Utilidades;
+
 import java.lang.reflect.Array;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  *
- * @author Santiago
+ * @author 
  */
 public class GrafosEtiquetadosDirigidos<E> extends GrafoDirigido {
 
     protected E[] labels;
     protected HashMap<E, Integer> dicVertices;
     private Class<E> clazz;
+    private Double[][] distancias;
 
     public GrafosEtiquetadosDirigidos(Integer num_vertices, Class clazz) {
         super(num_vertices);
@@ -70,7 +75,7 @@ public class GrafosEtiquetadosDirigidos<E> extends GrafoDirigido {
         }
     }
 
-    public DynamicList<Adyacencia> adjacents(E label) throws Exception {
+    public ListaEnlazada<Adyacencia> adjacents(E label) throws Exception {
         if (isAllLabelsGraph()) {
             return adyacentes(getVerticeE(label));
         } else {
@@ -100,7 +105,7 @@ public class GrafosEtiquetadosDirigidos<E> extends GrafoDirigido {
         try {
             for (int i = 1; i <= num_vertice(); i++) {
                 grafo.append("[").append(i).append("] ").append(getLabelE(i) + "\n");
-                DynamicList<Adyacencia> list = adyacentes(i);
+                ListaEnlazada<Adyacencia> list = adyacentes(i);
                 for (int j = 0; j < list.getLength(); j++) {
                     Adyacencia a = list.getInfo(j);
                     grafo.append("ady [").append(a.getDestino()).append("] = ").append(getLabelE(a.getDestino())).append(" " + a.getPeso()).append("\n");
@@ -109,6 +114,185 @@ public class GrafosEtiquetadosDirigidos<E> extends GrafoDirigido {
         } catch (Exception e) {
         }
         return grafo.toString();
+    }
+     public String aplicarAlgoritmoBellmanFord(int nodoOrigen) throws Exception {
+        inicializarDistancias(nodoOrigen);
+
+        for (int i = 1; i <= num_vertice() - 1; i++) {
+            boolean hayCambios = false;
+            for (int u = 1; u <= num_vertice(); u++) {
+                for (int v = 1; v <= num_vertice(); v++) {
+                    if (existe_arista(u, v)) {
+                        double pesoUV = peso_arista(u, v);
+                        if (distancias[nodoOrigen][u] + pesoUV < distancias[nodoOrigen][v]) {
+                            distancias[nodoOrigen][v] = distancias[nodoOrigen][u] + pesoUV;
+                            hayCambios = true;
+                        }
+                    }
+                }
+            }
+            if (!hayCambios) {
+                break;
+            }
+        }
+
+        for (int u = 1; u <= num_vertice(); u++) {
+            for (int v = 1; v <= num_vertice(); v++) {
+                if (existe_arista(u, v)) {
+                    double pesoUV = peso_arista(u, v);
+                    if (distancias[nodoOrigen][u] + pesoUV < distancias[nodoOrigen][v]) {
+                        throw new Exception("Hay ciclo negativo");
+                    }
+                }
+            }
+        }
+
+        return construirRepresentacionMatrizDistancias();
+    }
+
+    private void inicializarDistancias(int nodoOrigen) throws Exception {
+        distancias = new Double[num_vertice() + 1][num_vertice() + 1];
+        for (int i = 1; i <= num_vertice(); i++) {
+            for (int j = 1; j <= num_vertice(); j++) {
+                if (i == j) {
+                    distancias[i][j] = 0.0;
+                } else if (existe_arista(i, j)) {
+                    distancias[i][j] = peso_arista(i, j);
+                } else {
+                    distancias[i][j] = Double.POSITIVE_INFINITY;
+                }
+            }
+        }
+
+        for (int i = 1; i <= num_vertice(); i++) {
+            if (i != nodoOrigen) {
+                distancias[nodoOrigen][i] = Double.POSITIVE_INFINITY;
+            }
+        }
+    }
+
+    private String construirRepresentacionMatrizDistancias() {
+        StringBuilder representacion = new StringBuilder();
+
+        for (int i = 1; i <= num_vertice(); i++) {
+            for (int j = 1; j <= num_vertice(); j++) {
+                if (distancias[i][j] == Double.POSITIVE_INFINITY) {
+                    representacion.append("INF ");
+                } else {
+                    representacion.append(String.format("%.2f ", distancias[i][j]));
+                }
+            }
+            representacion.append("\n");
+        }
+
+        return representacion.toString();
+    }
+
+    // metodo de Floyd
+    public String aplicarAlgoritmoFloydConEtiquetas() throws Exception {
+        inicializarDistancias();
+
+        for (int k = 1; k <= num_vertice(); k++) {
+            for (int i = 1; i <= num_vertice(); i++) {
+                for (int j = 1; j <= num_vertice(); j++) {
+                    if (distancias[i][k] != Double.POSITIVE_INFINITY && distancias[k][j] != Double.POSITIVE_INFINITY) {
+                        double nuevaDistancia = distancias[i][k] + distancias[k][j];
+                        if (nuevaDistancia < distancias[i][j]) {
+                            distancias[i][j] = nuevaDistancia;
+                        }
+                    }
+                }
+            }
+        }
+
+        return construirRepresentacionMatrizDistancias();
+    }
+
+    private void inicializarDistancias() throws Exception {
+        distancias = new Double[num_vertice() + 1][num_vertice() + 1];
+        for (int i = 1; i <= num_vertice(); i++) {
+            for (int j = 1; j <= num_vertice(); j++) {
+                if (i == j) {
+                    distancias[i][j] = 0.0;
+                } else if (existe_arista(i, j)) {
+                    distancias[i][j] = peso_arista(i, j);
+                } else {
+                    distancias[i][j] = Double.POSITIVE_INFINITY;
+                }
+            }
+        }
+    }
+
+    private String construiRepresentacionMatrizDistancias() {
+        StringBuilder representacion = new StringBuilder();
+
+        for (int i = 1; i <= num_vertice(); i++) {
+            for (int j = 1; j <= num_vertice(); j++) {
+                if (distancias[i][j] == Double.POSITIVE_INFINITY) {
+                    representacion.append("INF ");
+                } else {
+                    representacion.append(String.format("%.2f ", distancias[i][j]));
+                }
+            }
+            representacion.append("\n");
+        }
+
+        return representacion.toString();
+    }
+
+    //metodo de Anchura 
+    public boolean BFS(Integer verticeInicial) throws Exception {
+        boolean[] visitados = new boolean[num_vertice() + 1];
+        Queue<Integer> cola = new LinkedList<>();
+        cola.add(verticeInicial);
+
+        while (!cola.isEmpty()) {
+            Integer verticeActual = cola.poll();
+            visitados[verticeActual] = true;
+
+            ListaEnlazada<Adyacencia> adyacentes = adyacentes(verticeActual);
+            for (int i = 0; i < adyacentes.getLength(); i++) {
+                Integer vecino = adyacentes.getInfo(i).getDestino();
+                if (!visitados[vecino]) {
+                    cola.add(vecino);
+                }
+            }
+        }
+
+        for (int i = 1; i <= num_vertice(); i++) {
+            if (!visitados[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //metodo Profundidad
+    public boolean DFS(Integer verticeInicial) throws Exception {
+        boolean[] visitados = new boolean[num_vertice() + 1];
+        Stack<Integer> pila = new Stack<>();
+        pila.push(verticeInicial);
+
+        while (!pila.isEmpty()) {
+            Integer verticeActual = pila.pop();
+            if (!visitados[verticeActual]) {
+                visitados[verticeActual] = true;
+                ListaEnlazada<Adyacencia> adyacentes = adyacentes(verticeActual);
+                for (int i = 0; i < adyacentes.getLength(); i++) {
+                    Integer vecino = adyacentes.getInfo(i).getDestino();
+                    if (!visitados[vecino]) {
+                        pila.push(vecino);
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= num_vertice(); i++) {
+            if (!visitados[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) throws Exception {
@@ -127,7 +311,7 @@ public class GrafosEtiquetadosDirigidos<E> extends GrafoDirigido {
         PaintGraph p = new PaintGraph();
         p.updateFile(ged);
         String url = "d3/grafo.js";
-        Utiles.abrirNavegadorPredeterminado(url);
+        Utilidades.abrirNavegadorPredeterminado(url);
         System.out.println(ged.toString());
     }
 }
---
+++ src/vista/listas/util/Utilvista.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista.listas.util;
+
+import controlador.TDA.listas.Exception.EmptyException;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author santi
+ */
+public class Utilvista {
+    
+//    public static void cargarcomboRolesL(JComboBox cbx) throws EmptyException{
+//        RolControl rc = new RolControl();
+//        cbx.removeAllItems();
+//        if (rc.getRoles().isEmpty()) {
+//            JOptionPane.showMessageDialog(null, "Lista vacia");
+//        }else{
+//            for (int i = 0; i < rc.getRoles().getLength(); i++) {
+//                cbx.addItem(rc.getRoles().getInfo(i));
+//            }
+//        }
+//    }
+    
+//    public static void cargarcomboRoles(JComboBox cbx) throws EmptyException{
+//        RolControl rc = new RolControl();
+//        cbx.removeAllItems();
+//        for (Integer i = 0; i < rc.getRoles().getLength(); i++) {
+//            System.out.println(rc.getRoles().getLength());
+//            System.out.println(rc.getRoles().getInfo(i).getNombre());
+//            cbx.addItem(rc.getRoles().getInfo(i));
+//        }
+//    }
+//    
+//    public static Rol obtenerRolControl(JComboBox cbx){
+//        return (Rol) cbx.getSelectedItem();
+//    }
+}
